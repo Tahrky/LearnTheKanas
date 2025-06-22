a187d28b581d55214bcfe4f6c699918f4a78372e
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2025-06-22 22:50:05 +0200

    Add : Liaison du projet avec une base de données SQL Server.      - Premier commit d’initialisation de la base de données avec une seule table : l’utilisateur

---
+++ DAL/KanaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ApprentissageKana.DAL
+{
+    public class KanaContext : DbContext
+    {
+        public DbSet<ApprentissageKana.Models.Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-EJF1JS3V;Initial Catalog=LearnTheKana;Integrated Security=SSPI;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApprentissageKana.Models
+{
+    public class Users
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public DateTime creationAccountDate { get; set; }
+        public DateTime lastConnectionDate { get; set; }
+    }
+}
